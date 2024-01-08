ea727aba204a6d1efefd8eeea18e4bb03ca990af
leehandsub <gustjq7027@naver.com>
2024-01-08 22:25:23 +0900

    refact : 코드 정리

--- app/src/main/java/com/example/simplegallery/model/Photo.kt
+++ app/src/main/java/com/example/simplegallery/model/Photo.kt
@@ -4,11 +4,21 @@ import com.google.gson.annotations.SerializedName
 
 
 data class Photo(
+    @SerializedName("author")
     val author: String,
+
     @SerializedName("download_url")
     val downloadUrl: String,
+
+    @SerializedName("height")
     val height: Int,
+
+    @SerializedName("id")
     val id: String,
+
+    @SerializedName("url")
     val url: String,
+
+    @SerializedName("width")
     val width: Int,
 )
