5bdabe1c74fafad913957c297165ef2488632672
leehandsub <gustjq7027@naver.com>
2024-01-10 22:54:22 +0900

    refact : 레트로핏 객체를 만드는 것과 객체에 대한 관심사 분리

--- app/src/main/java/com/example/simplegallery/network/RetrofitUtil.kt
+++ app/src/main/java/com/example/simplegallery/network/RetrofitUtil.kt
@@ -1,31 +1,9 @@
 package com.example.simplegallery.network
 
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
+import com.example.simplegallery.ApplicationClass
 
-object RetrofitUtil {
-    private const val SERVER_URL = "https://picsum.photos/"
-    val api: PhotoApi by lazy {
-        createMealsApi()
-    }
-
-    private fun createMealsApi(): PhotoApi {
-        val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-        val okHttpClient = OkHttpClient.Builder()
-            .readTimeout(5000, TimeUnit.MILLISECONDS)
-            .writeTimeout(5000,TimeUnit.MILLISECONDS)
-            .connectTimeout(5000, TimeUnit.MILLISECONDS)
-            .addInterceptor(loggingInterceptor)
-            .build()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(SERVER_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(okHttpClient)
-            .build()
-        return retrofit.create(PhotoApi::class.java)
+class RetrofitUtil {
+    companion object {
+        val photoApi: PhotoApi = ApplicationClass.retrofit.create(PhotoApi::class.java)
     }
 }
