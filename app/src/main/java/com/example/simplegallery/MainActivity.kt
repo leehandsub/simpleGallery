88073918cc860ad1728dc92a7b8029777a040513
leehandsub <gustjq7027@naver.com>
2024-02-15 22:49:07 +0900

    feat : di예시를 위한 구조 변경

--- app/src/main/java/com/example/simplegallery/MainActivity.kt
+++ app/src/main/java/com/example/simplegallery/MainActivity.kt
@@ -3,27 +3,33 @@ package com.example.simplegallery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.simplegallery.model.Photo
+import com.example.simplegallery.repository.PhotoListRemoteDataSource
+import com.example.simplegallery.repository.PhotoListRepository
 import com.example.simplegallery.ui.screen.photodetail.PhotoDetailScreen
 import com.example.simplegallery.ui.screen.photolist.PhotoListScreen
 import com.example.simplegallery.ui.screen.photolist.PhotoListViewModel
 import com.example.simplegallery.ui.theme.SimpleGalleryTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var photoListViewModel: PhotoListViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val photoListViewModel: PhotoListViewModel by viewModels()
+        //val photoListViewModel: PhotoListViewModel by viewModels()
+
+        val photoListRemoteDataSource = PhotoListRemoteDataSource()
+        val photoListRepository = PhotoListRepository(photoListRemoteDataSource)
+        photoListViewModel = PhotoListViewModel(photoListRepository)
+
         photoListViewModel.getPhotos()
 
         setContent {
