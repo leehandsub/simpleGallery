e9fe838c1bd2b923d2aec2bd2d5c2eb99db98eac
leehandsub <35682233+leehandsub@users.noreply.github.com>
2024-02-19 23:58:55 +0900

    Revert "FEAT : di를 위한 준비"

--- app/src/main/java/com/example/simplegallery/MainActivity.kt
+++ app/src/main/java/com/example/simplegallery/MainActivity.kt
@@ -3,30 +3,27 @@ package com.example.simplegallery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.simplegallery.model.Photo
-import com.example.simplegallery.ui.common.AppContainer
 import com.example.simplegallery.ui.screen.photodetail.PhotoDetailScreen
 import com.example.simplegallery.ui.screen.photolist.PhotoListScreen
 import com.example.simplegallery.ui.screen.photolist.PhotoListViewModel
 import com.example.simplegallery.ui.theme.SimpleGalleryTheme
 
 class MainActivity : ComponentActivity() {
-    private lateinit var photoListViewModel: PhotoListViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //val photoListViewModel: PhotoListViewModel by viewModels()
-
-        photoListViewModel = AppContainer.photoListViewModelFactory.create()
-
+        val photoListViewModel: PhotoListViewModel by viewModels()
         photoListViewModel.getPhotos()
 
         setContent {
